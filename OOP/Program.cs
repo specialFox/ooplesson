b0272511cc448d92c5fa6466fd26d8adb24e1a04
specialFox <jcreanna@gmail.com>
2017-05-19 01:49:36 +0300

    List using

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -12,9 +12,24 @@ namespace OOP
         {
 
             Point p1 = new Point(1,3,'*');
-            p1.Draw();
             Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            Point p3 = new Point(6, 4, '@');
+            Point p4 = new Point(8, 6, '$');
+            Point p5 = new Point(9, 8, '%');
+            Point p6 = new Point(12, 7, '^');
+
+            List<Point> lPoint = new List<Point>();
+            lPoint.Add(p1);
+            lPoint.Add(p2);
+            lPoint.Add(p3);
+            lPoint.Add(p4);
+            lPoint.Add(p5);
+            lPoint.Add(p6);
+
+            foreach (Point i in lPoint) {
+                i.Draw();
+                //Console.WriteLine(i);
+            }
 
             Console.ReadLine();
         }
