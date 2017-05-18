50767e9b2dbc10b8a3b8ec3345315aecb357e60d
specialFox <jcreanna@gmail.com>
2017-05-19 01:56:51 +0300

    Create class for line

---
+++ OOP/HorizontalLine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class HorizontalLine
+    {
+        List<Point> pLine;
+        public HorizontalLine(int xLeft,int xRight, int y,char sum) {
+
+            for (int x = xLeft; x <= xRight; x++) {
+                pLine.Add(new Point(x, y, sum));
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in pLine) {
+                p.Draw();
+            }
+        }
+    }
+}
