0716d2ee599317b8a64fc2561924064baaede168
specialFox <jcreanna@gmail.com>
2017-05-19 01:31:50 +0300

    Incapsulation

--- OOP/Point.cs
+++ OOP/Point.cs
@@ -6,22 +6,34 @@ using System.Threading.Tasks;
 
 namespace OOP
 {
+    //вывод символа на экран по указанной позиции
     class Point
     {
 
         public int x, y;
         public char sym;
 
-        //вывод символа на экран по указанной позиции
-        public void Draw(int xP, int yP, char symP)
+        public Point()
         {
-            Console.SetCursorPosition(xP, yP);
-            Console.Write(symP);
+            //Console.Write("constructor");
+        }
+
+        public Point(int _x,int _y, char _sym) {
+            x = _x;
+            y = _y;
+            sym = _sym;
+            //Console.Write("constructor");
         }
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
+        }
+        
+        public void Draw(int _x, int _y, char _sym)
+        {
+            Console.SetCursorPosition(_x, _y);
+            Console.Write(_sym);
         }
     }
 
