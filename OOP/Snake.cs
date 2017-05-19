218d67beb4ed12526312a3d55879a5b1d366af3d
specialFox <jcreanna@gmail.com>
2017-05-19 04:28:58 +0300

    Final

--- OOP/Snake.cs
+++ OOP/Snake.cs
@@ -68,6 +68,16 @@ namespace OOP
             else return false;
         }
 
+        public bool HitHisTail() {
+            Point head = pList.Last();
+            for (int i = 0; i < pList.Count()-2; i++) {
+                if (head.IsHit(pList[i])) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
 
     }
 }
